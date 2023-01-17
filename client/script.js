1517ceb873ddffd4c1bc34fb954779c9f3958629
claude17 <claude.mta@gmail.com>
2023-01-17 21:13:36 +0600

    update script.js

--- client/script.js
+++ client/script.js
@@ -87,7 +87,7 @@ const handleSubmit = async (e) => {
     loader(messageDiv)
 
     const response = await fetch('https://s4z.onrender.com/', {
-        mode: 'cors',
+        mode: 'no-cors',
         credentials: 'include',
         method: 'POST',
         headers: {
